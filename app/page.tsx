c4567a98aedb508c725ca385b96856f4e13c4289
Antoine ROBIN <antoine.robin@aticommunity.fr>
2024-08-02 00:01:49 +0200

    feat(component): remove maintenance component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import NavBar from "@/components/navbar/navbar";
 import Footer from "@/components/Footer/Footer";
 import Content from "@/components/Content/content";
-import Maintenance from "@/components/Maintenance/maintenance";
 
 export default function Home() {
     return (
         <>
-            <Maintenance />
             <NavBar />
             <Content />
             <Footer />
