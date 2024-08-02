cbd0d80a591fefca5ee2322aa35fbc641f9c742b
Antoine ROBIN <antoine.robin@aticommunity.fr>
2024-08-02 13:06:32 +0200

    fix(navbar): reverse theme switcher icons

--- components/Navbar/navbar-theme.tsx
+++ components/Navbar/navbar-theme.tsx
@@ -11,7 +11,7 @@ export default function NavBarTheme() {
 
                 {/* sun icon */}
                 <svg
-                    className="swap-off h-7 w-7 fill-current"
+                    className="swap-on h-7 w-7 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                 >
@@ -20,7 +20,7 @@ export default function NavBarTheme() {
 
                 {/* moon icon */}
                 <svg
-                    className="swap-on h-7 w-7 fill-current"
+                    className="swap-off h-7 w-7 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                 >
