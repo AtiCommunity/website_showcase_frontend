20617979f373862109fd122f9b0ce1a00c364d24
Antoine ROBIN <antoine.robin@aticommunity.fr>
2024-08-04 16:36:08 +0200

    refactor(profile card): reduce y padding

--- components/ProfileCard/profilecard.tsx
+++ components/ProfileCard/profilecard.tsx
@@ -5,19 +5,19 @@ import Link from "next/link";
 const ProfileCard = () => {
     return (
         <>
-            <div className="container bg-blue-900 text-amber-400 mx-auto max-w-md card glass w-96">
+            <div className="bg-blue-900 text-amber-400 mx-auto max-w-md card glass w-72">
                 <figure>
                     <Image src={profilePic} alt="profile" />
                 </figure>
-                <div className="card-body">
+                <div className="card-body px-6 py-2">
                     <h2 className="card-title">Antoine (Atineon) ROBIN</h2>
                     <p>
                         IT Engineer{" "}
                         <span className="badge badge-error">Apprentice</span>
                     </p>
-                    <div className="card-actions justify-end">
+                    <div className="card-actions justify-center">
                         <Link
-                            className="btn btn-primary"
+                            className="btn btn-secondary"
                             href="https://antoinerobin.fr"
                             target="_blank"
                         >
