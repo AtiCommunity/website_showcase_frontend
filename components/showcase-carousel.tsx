59dfb0395020d986876a168dda27752704c33a7f
Antoine ROBIN <antoine.robin@aticommunity.fr>
2024-08-05 23:52:43 +0200

    feat(showcase carousel): add projects to the carousel

--- components/showcase-carousel.tsx
+++ components/showcase-carousel.tsx
@@ -1,48 +1,96 @@
+import { FaDiscord, FaRegFaceSmileWink } from "react-icons/fa6";
+import { MdOutlineWeb } from "react-icons/md";
+import { SiMinecraft } from "react-icons/si";
+
 const ShowcaseCarousel = () => {
     return (
         <>
-            <div className="carousel carousel-center space-x-4">
+            <div className="carousel max-w-full carousel-center space-x-4 px-10 rounded-box">
                 <div className="carousel-item">
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-                        className="rounded-box"
-                    />
+                    <div className="card glass w-80 max-w-md bg-blue-900 text-amber-400">
+                        <figure className="bg-base-100 min-h-80">
+                            <MdOutlineWeb fontSize={100} />
+                        </figure>
+                        <div className="card-body justify-around">
+                            <h2 className="card-title">
+                                <p>Showcase website</p>
+                                <div className="badge badge-error">NEW</div>
+                            </h2>
+                            <p>
+                                This website are here to display my project with
+                                style.
+                            </p>
+                            <div className="flex">
+                                <p className="max-w-max">
+                                    Yes. This is a project{" "}
+                                </p>
+                                <div className="content-center ps-1">
+                                    <FaRegFaceSmileWink />
+                                </div>
+                            </div>
+
+                            <div className="card-actions justify-end">
+                                <div className="badge badge-outline">
+                                    JavaScript
+                                </div>
+                                <div className="badge badge-outline">
+                                    Next.js
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="carousel-item">
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
-                        className="rounded-box"
-                    />
+                    <div className="card glass w-80 max-w-md bg-blue-900 text-amber-400">
+                        <figure className="bg-base-100 min-h-80">
+                            <FaDiscord fontSize={100} />
+                        </figure>
+                        <div className="card-body justify-around">
+                            <h2 className="card-title">
+                                <p>TempChannels Discord BOT</p>
+                                <div className="badge badge-error">NEW</div>
+                            </h2>
+                            <p>
+                                The aim of this BOT is to create temporary
+                                channels for user.
+                            </p>
+
+                            <div className="card-actions justify-end">
+                                <div className="badge badge-outline">
+                                    JavaScript
+                                </div>
+                                <div className="badge badge-outline">
+                                    Discord.js
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="carousel-item">
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
-                        className="rounded-box"
-                    />
-                </div>
-                <div className="carousel-item">
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
-                        className="rounded-box"
-                    />
-                </div>
-                <div className="carousel-item">
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
-                        className="rounded-box"
-                    />
-                </div>
-                <div className="carousel-item">
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
-                        className="rounded-box"
-                    />
-                </div>
-                <div className="carousel-item">
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
-                        className="rounded-box"
-                    />
+                    <div className="card glass w-80 max-w-md bg-blue-900 text-amber-400">
+                        <figure className="bg-base-100 min-h-80">
+                            <SiMinecraft fontSize={200} />
+                        </figure>
+                        <div className="card-body justify-around">
+                            <h2 className="card-title">
+                                <p>Minecraft Network</p>
+                                <div className="badge badge-error">NEW</div>
+                            </h2>
+                            <p>
+                                The objective of this project to host multiple
+                                servers like a lobby, survival and creative
+                                servers.
+                            </p>
+
+                            <div className="card-actions justify-end">
+                                <div className="badge badge-outline">Java</div>
+                                <div className="badge badge-outline">
+                                    Velocity
+                                </div>
+                                <div className="badge badge-outline">Paper</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
