3f5be287398fc2f250a777e022f8dc39a3e8905b
Antoine ROBIN <antoine.robin@aticommunity.fr>
2024-12-25 00:48:35 +0000

    fix(fonts): add fonts file

---
+++ src/styles/fonts.tsx
@@ -0,0 +1,4 @@
+import { Akronim, KoHo } from "next/font/google";
+
+export const akronim = Akronim({ weight: ["400"], subsets: ["latin"] });
+export const koho = KoHo({ weight: ["400"], subsets: ["latin"] });
