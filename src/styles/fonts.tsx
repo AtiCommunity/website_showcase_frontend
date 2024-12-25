46aa128a0c315e9aeee852d374096e49eb624ffa
Antoine ROBIN <antoine.robin@aticommunity.fr>
2024-12-25 01:51:15 +0100

    fix(fonts): add needed fonts to font file

--- src/styles/fonts.tsx
+++ src/styles/fonts.tsx
@@ -1,4 +1,6 @@
-import { Akronim, KoHo } from "next/font/google";
+import { Akronim, Honk, KoHo, Nabla } from "next/font/google";
 
 export const akronim = Akronim({ weight: ["400"], subsets: ["latin"] });
+export const honk = Honk({ weight: ["400"], subsets: ["latin"] });
 export const koho = KoHo({ weight: ["400"], subsets: ["latin"] });
+export const nabla = Nabla({ weight: ["400"], subsets: ["latin"] });
